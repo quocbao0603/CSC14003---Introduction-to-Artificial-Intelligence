73d8f1354d966bd4c1d6f388f5dfec63730351bc
baoz <qbao0603@gmail.com>
2020-11-28 09:10:19 +0700

    Tran Clan full

---
+++ Lab02-Logic/Tran Clan/Result_query.pl
@@ -0,0 +1,73 @@
+1.
+X = hientu.
+2.
+X = tranminhtong ;
+X = trannghetong ;
+X = hoquyly ;
+X = trandutong ;
+X = tranhientong ;
+X = tranduetong ;
+X = hohanthuong ;
+X = tranthuantong ;
+X = tranphede ;
+X = duongnhatle ;
+X = tranthieude ;
+X = tranquangde.
+3.
+X = hientu.
+4.
+false.
+5.
+false.
+6.
+ false.
+7.
+false.
+8.
+false.
+9.
+ true .
+10.
+false.
+11.
+true.
+12.
+true.
+13.
+X = huyninh 
+14.
+X = tranngac ;
+X = giandinhde.
+15.
+false.
+16.
+X = tranminhtong ;
+X = hientu.
+17.
+false.
+18.
+X = tranminhtong.
+19.
+true.
+20.
+true.
+21.
+false.
+22.
+false.
+23.
+X = huyninh
+24.
+X = hientu
+25.
+false.
+26.
+X = tranminhton
+27.
+X = trandueton
+28.
+true.
+29.
+X = huyninh 
+30.
+false.
